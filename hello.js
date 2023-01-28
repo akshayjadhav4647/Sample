c47616c440536b85ada1b045ae7dd494b75e05b4
akshayjadhav4647 <akshayjadhav4647@gmail.com>
2023-01-28 14:12:08 +0530

    function added

---
+++ hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const hello = () => {
+  return (
+    <>
+      <h1> Hello World</h1>
+      <p> jhdudhnsjddioidhdmk;okpowl</p>
+      <a href="#">Click me</a>
+    </>
+  );
+};
+
+export default hello;
